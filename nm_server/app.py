866f64c9391631ca812f5023988469de9db09d29
joonyw <joonyw.lee@gmail.com>
2024-06-08 21:45:42 +0900

    .

--- nm_server/app.py
+++ nm_server/app.py
@@ -53,7 +53,7 @@ def complete():
 def get_medication():
     data = request.json
     rrn = data.get('rrn')
-    print(rrn)
+
     data = query_database_to_json(rrn)
     data = json.dumps(data, ensure_ascii=False)
 
@@ -253,11 +253,11 @@ def generate_pdf_from_json(data, output_pdf_path):
 
     doc.build(elements)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-# @app.route("/")
-# def index():
-#     return "<h1>Hello!</h1>"
-# if __name__ == "__main__":
-#     from waitress import serve
-#     serve(app, host="0.0.0.0", port=8080)
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5000)
+@app.route("/")
+def index():
+    return "<h1>Hello!</h1>"
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
