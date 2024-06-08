2beaaf16b0e125012589758aa067074ef3351b64
Seung-ho Kim <rhanfwlq980@naver.com>
2024-06-08 23:04:42 +0900

    chore: Clean up code and remove unused scroll controller

--- lib/screens/elder_page.dart
+++ lib/screens/elder_page.dart
@@ -23,6 +23,7 @@ class _ElderPageState extends State<ElderPage> {
   final FocusNode _rrnFirstFocusNode = FocusNode();
   final FocusNode _rrnSecondFocusNode = FocusNode();
   final FocusNode _phoneFocusNode = FocusNode();
+  final ScrollController _scrollController = ScrollController();
 
   bool _isNameEntered = false;
   bool _isRrnEntered = false;
@@ -44,6 +45,7 @@ class _ElderPageState extends State<ElderPage> {
     _rrnFirstFocusNode.dispose();
     _rrnSecondFocusNode.dispose();
     _phoneFocusNode.dispose();
+    _scrollController.dispose();
     super.dispose();
   }
 
@@ -90,6 +92,7 @@ class _ElderPageState extends State<ElderPage> {
 
   void _requestPhoneFocus() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollToTop();
       FocusScope.of(context).requestFocus(_phoneFocusNode);
     });
   }
@@ -103,96 +106,15 @@ class _ElderPageState extends State<ElderPage> {
     }
   }
 
-  // void _showAgreementModal() {
-  //   showModalBottomSheet(
-  //     context: context,
-  //     isScrollControlled: true,
-  //     builder: (BuildContext context) {
-  //       return StatefulBuilder(
-  //         builder: (BuildContext context, StateSetter setState) {
-  //           return Container( 
-  //             height: MediaQuery.of(context).size.height * 0.3, 
-  //             child: Column(
-  //               children: [
-  //                 Padding(
-  //                   padding: const EdgeInsets.all(16.0),
-  //                   child: Row(
-  //                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //                     children: [
-  //                       Text(
-  //                         '서비스 약관 동의',
-  //                         style: TextStyle(
-  //                           fontSize: 24,
-  //                           fontWeight: FontWeight.bold,
-  //                         ),
-  //                       ),
-  //                       IconButton(
-  //                         icon: Icon(Icons.close),
-  //                         onPressed: () {
-  //                           Navigator.of(context).pop();
-  //                         },
-  //                       ),
-  //                     ],
-  //                   ),
-  //                 ),
-  //                 Expanded(
-  //                   child: SingleChildScrollView(
-  //                     child: ListBody(
-  //                       children: <Widget>[
-  //                         CheckboxListTile(
-  //                           controlAffinity: ListTileControlAffinity.leading,
-  //                           title: Text('개인정보이용 동의'),
-  //                           value: _isChecked1,
-  //                           onChanged: (value) {
-  //                             setState(() {
-  //                               _isChecked1 = value ?? false;
-  //                             });
-  //                           },
-  //                         ),
-  //                         CheckboxListTile(
-  //                           controlAffinity: ListTileControlAffinity.leading,
-  //                           title: Text('고유식별정보처리 동의'),
-  //                           value: _isChecked2,
-  //                           onChanged: (value) {
-  //                             setState(() {
-  //                               _isChecked2 = value ?? false;
-  //                             });
-  //                           },
-  //                         ),
-  //                         // ... (다른 약관 추가) ...
-  //                       ],
-  //                     ),
-  //                   ),
-  //                 ),
-  //                 Padding(
-  //                   padding: const EdgeInsets.all(16.0),
-  //                   child: ElevatedButton(
-  //                     style: ElevatedButton.styleFrom(
-  //                       minimumSize: Size(double.infinity, 60),
-  //                     ),
-  //                     onPressed: () {
-  //                       setState(() {
-  //                         _isChecked1 = true;
-  //                         _isChecked2 = true;
-  //                         // ... (다른 약관 전체 선택) ...
-  //                       });
-  //                       Navigator.of(context).pop();
-  //                     },
-  //                     child: Text(
-  //                       '전체 동의',
-  //                       style: TextStyle(fontSize: 24),
-  //                     ),
-  //                   ),
-  //                 ),
-  //               ],
-  //             ),
-  //           );
-  //         },
-  //       );
-  //     },
-  //   );
-  // }
-
+  void _scrollToTop() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.animateTo(
+        0,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
 
   void _showAgreementModal() {
   showModalBottomSheet(
@@ -202,7 +124,7 @@ class _ElderPageState extends State<ElderPage> {
       return StatefulBuilder(
         builder: (BuildContext context, StateSetter setState) {
           return Container(
-            height: MediaQuery.of(context).size.height * 0.3,
+            height: MediaQuery.of(context).size.height * 0.5,
             child: Column(
               children: [
                 Padding(
@@ -342,6 +264,7 @@ class _ElderPageState extends State<ElderPage> {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
+        controller: _scrollController,
         padding: EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
